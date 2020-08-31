b036ee5e340c9564fe88eddddd91fda6f0f017af
Robert Bergers <bergersr97@gmail.com>
2020-08-31 16:23:33 -0400

    Fill in description content

--- src/react/components/sections/Description.js
+++ src/react/components/sections/Description.js
@@ -1,6 +1,5 @@
 /* Packages */
 import React from 'react';
-import {connect} from 'react-redux';
 
 
 /* Component */
@@ -13,7 +12,13 @@ export class Description extends React.Component {
     render() {
         return (
             <div id="Description" className="Description">
-                
+                <h1 className="Description__title">
+                    The Open Source Computer Science Degree
+                </h1>
+
+                <p className="Description__description">
+                    This is a curated list of free courses from reputable universities like MIT, Stanford, and Princeton that satisfy the same requirements as an undergraduate Computer Science degree, minus general education.
+                </p>
             </div>
         );
     }
